2d9eca9e7eac99a318243291d5c0dc962a130a6f
Den <butdda@mail.ru>
2024-06-27 19:39:24 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -3,18 +3,23 @@ import Button from "../Button/Button";
 
 function ProductItem(props) {
     const onAddHandler = () => {
-        props.onAdd(props.product.id);
+        props.onAdd(props.item);
     };
 
     return (
         <div className="product">
             <img className="img" alt="tire" src={props.item.mainPhoto}></img>
-            <h3 className="title">{props.item.title} id:{props.item.id}</h3>
+            <h3 className="title">
+                {props.item.title} id:{props.item.id}
+            </h3>
             <div className="season">Сезон: {props.item.season}</div>
             <div className="size">
-                Размер: {props.item.width}/{props.item.hight}/r{props.item.diameter}
+                Размер: {props.item.width}/{props.item.hight}/r
+                {props.item.diameter}
             </div>
-            <div className="discription">Описание: {props.item.discription}</div>
+            <div className="discription">
+                Описание: {props.item.discription}
+            </div>
             <div className="price">
                 <span>
                     Цена: <b>{props.item.price} руб.</b>
